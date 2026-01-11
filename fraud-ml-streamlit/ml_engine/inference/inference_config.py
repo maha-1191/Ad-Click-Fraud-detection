65b351b85658aa8772146db0850cb716b1b2c45a
Maha Lakshmi <mahabml111@gmail.com>
2026-01-11 22:22:11 +0530

    updated inference_config

--- fraud-ml-streamlit/ml_engine/inference/inference_config.py
+++ fraud-ml-streamlit/ml_engine/inference/inference_config.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 
 class InferenceConfig:
-    """
-    Lightweight config for inference-only deployment
-    """
-
     BASE_DIR = Path(__file__).resolve().parents[2]
 
-    # Where trained models are stored
-    MODEL_DIR = BASE_DIR / "ml_engine" / "models"
+    # POINT TO DJANGO ARTIFACTS (SOURCE OF TRUTH)
+    MODEL_DIR = (
+        BASE_DIR
+        / ".."
+        / ".."
+        / "fraudapp"
+        / "ml_engine"
+        / "artifacts"
+    )
 
-    # Must match training-time sequence length
     SEQUENCE_LENGTH = 10
-
