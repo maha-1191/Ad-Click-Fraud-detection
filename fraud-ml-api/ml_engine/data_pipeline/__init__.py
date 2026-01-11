ff8542d9e858a8f381a83be0270d10a94615d595
Maha Lakshmi <mahabml111@gmail.com>
2026-01-11 12:12:44 +0530

    removed unnecessary files

--- fraud-ml-api/ml_engine/data_pipeline/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_validation import DataValidator, DataValidationException
-from .preprocessing import preprocess_data
-from .features import build_features
-from .sequence_builder import build_sequences
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-python manage.py migrate
-python manage.py collectstatic --noinput
-gunicorn ad_click_fraud_classification.wsgi:application
